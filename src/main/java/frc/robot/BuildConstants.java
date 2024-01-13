3d207c1758166afaf405656619989c2b00ea53fc
Youngmoo Kim <ykim@drexel.edu>
2024-01-13 12:16:10 -0500

    Starting with AdvantageKit's AdvancedSwerveDriveProject template
    2024-01-13: Starting with  AdvancedSwerveDriveProject template from AdvantageKit release 3.0.0. Downloaded from:
    https://github.com/Mechanical-Advantage/AdvantageKit/releases/tag/v3.0.0

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_AdvancedSwerveDriveProject";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2024-01-13 11:58:56 EST";
+  public static final long BUILD_UNIX_TIME = 1705165136805L;
+  public static final int DIRTY = 143;
+
+  private BuildConstants() {}
+}

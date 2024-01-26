dee3b55ec573bc7b5acd8844a4d3ba84d8b6ee69
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-01-25 19:52:59 -0500

    added arm code; temp removal of photogate
    Still need to figure out how to ensure brakemode is enabled on talons.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "cf23f50f1f2b31404b68e83fb0697eb661891850";
-  public static final String GIT_DATE = "2024-01-23 10:01:56 EST";
+  public static final int GIT_REVISION = 11;
+  public static final String GIT_SHA = "506cc9b33067bbfcf6b72701108742355b650918";
+  public static final String GIT_DATE = "2024-01-23 20:06:00 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-23 20:01:18 EST";
-  public static final long BUILD_UNIX_TIME = 1706058078247L;
+  public static final String BUILD_DATE = "2024-01-25 19:40:36 EST";
+  public static final long BUILD_UNIX_TIME = 1706229636899L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/WinchSubsystem.java
+++ src/main/java/frc/robot/subsystems/WinchSubsystem.java
@@ -1,13 +1,26 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkLowLevel.MotorType;
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class WinchSubsystem extends SubsystemBase {
-  CANSparkMax winchMotor = new CANSparkMax(15, MotorType.kBrushless);
+  private final TalonFX m_ArmMotor1 = new TalonFX(15, "rio");
+
+  public WinchSubsystem() {
+    var config = new MotorOutputConfigs();
+    config.NeutralMode = NeutralModeValue.Brake;
+    m_ArmMotor1.getConfigurator().apply(config);
+  }
 
   public void setWinchSpeed(double speed) {
-    winchMotor.set(speed);
+    m_ArmMotor1.set(speed);
+  }
+
+  public void periodic() {
+    SmartDashboard.putNumber("Arm Encoder Value", m_ArmMotor1.getPosition().getValueAsDouble());
   }
 }

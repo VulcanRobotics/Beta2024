9e7c7358f50128dd3be2fd3602e77aed09e0ba4c
MakerEye <ckaucher@sch.org>
2024-01-27 10:39:09 -0500

    Random VSCode gremlins

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "Beta2024";
+  public static final String MAVEN_NAME = "Beta2024_backup";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "506cc9b33067bbfcf6b72701108742355b650918";
-  public static final String GIT_DATE = "2024-01-23 20:06:00 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "b6ddeaec62f1f601d79187a6d88dc4847f1f1ef7";
+  public static final String GIT_DATE = "2024-01-25 20:04:01 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-25 19:40:36 EST";
-  public static final long BUILD_UNIX_TIME = 1706229636899L;
+  public static final String BUILD_DATE = "2024-01-27 10:38:19 EST";
+  public static final long BUILD_UNIX_TIME = 1706369899542L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

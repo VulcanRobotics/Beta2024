6ab1ce612df4ed58cbaa77332dcc9240d5d312df
Youngmoo Kim <ykim@drexel.edu>
2024-01-28 12:24:50 -0500

    Merge Sat 1/27 updates to Main
    Merged in changes for arm, climb, and other updates from Sat. session.
    
    Main branch still uses old Drive system (with no vision).

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -23,7 +23,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.ClimbCommands;
 import frc.robot.commands.DispenseCommand;
 import frc.robot.commands.DriveCommands;

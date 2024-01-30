1ccc1c167905ec524830cc8eb624aedcdfe76fd9
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-01-30 15:00:36 -0500

    Implemented better arm position control (motion magic)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 19;
-  public static final String GIT_SHA = "dc72a6cf5a064b1c4c35bf4e3b49c43b99798a7a";
-  public static final String GIT_DATE = "2024-01-27 16:25:24 EST";
-  public static final String GIT_BRANCH = "PhotonVision";
-  public static final String BUILD_DATE = "2024-01-30 20:08:21 EST";
-  public static final long BUILD_UNIX_TIME = 1706663301182L;
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "6ab1ce612df4ed58cbaa77332dcc9240d5d312df";
+  public static final String GIT_DATE = "2024-01-28 12:24:50 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-01-30 14:57:09 EST";
+  public static final long BUILD_UNIX_TIME = 1706644629465L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

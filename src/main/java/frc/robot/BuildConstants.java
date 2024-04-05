93fa2abbe454f5bdd5e0ff3e1885a317d7930e5d
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-04-05 15:03:40 -0400

    changes for daisy auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 132;
-  public static final String GIT_SHA = "8f9a12698f6dedf09663683ebf084ff95a732afe";
-  public static final String GIT_DATE = "2024-04-05 11:10:02 EDT";
+  public static final int GIT_REVISION = 133;
+  public static final String GIT_SHA = "c0f7e83aa4fd559bbfc29fb33af108fec46905a0";
+  public static final String GIT_DATE = "2024-04-05 11:27:33 EDT";
   public static final String GIT_BRANCH = "VisionIO-PreLehigh";
-  public static final String BUILD_DATE = "2024-04-05 11:16:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1712330174529L;
+  public static final String BUILD_DATE = "2024-04-05 15:02:56 EDT";
+  public static final long BUILD_UNIX_TIME = 1712343776296L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

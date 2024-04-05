c0f7e83aa4fd559bbfc29fb33af108fec46905a0
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-04-05 11:27:33 -0400

    removed trap command, add it back later

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 131;
-  public static final String GIT_SHA = "a417ebcfad6bb6932153655adfdf4a118bb5ed8a";
-  public static final String GIT_DATE = "2024-04-05 09:16:42 EDT";
+  public static final int GIT_REVISION = 132;
+  public static final String GIT_SHA = "8f9a12698f6dedf09663683ebf084ff95a732afe";
+  public static final String GIT_DATE = "2024-04-05 11:10:02 EDT";
   public static final String GIT_BRANCH = "VisionIO-PreLehigh";
-  public static final String BUILD_DATE = "2024-04-05 10:24:55 EDT";
-  public static final long BUILD_UNIX_TIME = 1712327095924L;
+  public static final String BUILD_DATE = "2024-04-05 11:16:14 EDT";
+  public static final long BUILD_UNIX_TIME = 1712330174529L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

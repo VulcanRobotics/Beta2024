fd544c2e5ac3ff2ed46f72ecc741e3d40a4c9995
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-04-07 10:38:37 -0400

    Final changes from Lehigh
    mostly the autos that were created for elims

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 134;
-  public static final String GIT_SHA = "93fa2abbe454f5bdd5e0ff3e1885a317d7930e5d";
-  public static final String GIT_DATE = "2024-04-05 15:03:40 EDT";
+  public static final int GIT_REVISION = 135;
+  public static final String GIT_SHA = "f121295c5ee503754813349ebc0fa5773df60555";
+  public static final String GIT_DATE = "2024-04-06 11:58:12 EDT";
   public static final String GIT_BRANCH = "VisionIO-PreLehigh";
-  public static final String BUILD_DATE = "2024-04-06 09:58:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1712411903548L;
+  public static final String BUILD_DATE = "2024-04-06 15:35:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1712432115845L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}

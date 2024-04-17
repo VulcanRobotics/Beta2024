4192c9b2ce3b36f802cf49082e0e0771fd6ee94c
sgupte45 <58490916+ShinyKyogre@users.noreply.github.com>
2024-04-17 09:32:06 -0500

    added more source side options for auto
    Hopefully we can test these in practice match. We should also cleanse pathplanner of all dead autos to make technician job easier.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Beta2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 140;
-  public static final String GIT_SHA = "8a553f5880092e61da26586ccf1cd0e580312743";
-  public static final String GIT_DATE = "2024-04-15 15:38:05 EDT";
+  public static final int GIT_REVISION = 141;
+  public static final String GIT_SHA = "7d3a6316fe2fd9622894d8e45e3e1b64aedfd62e";
+  public static final String GIT_DATE = "2024-04-16 19:23:34 EDT";
   public static final String GIT_BRANCH = "Houston";
-  public static final String BUILD_DATE = "2024-04-16 19:13:20 EDT";
-  public static final long BUILD_UNIX_TIME = 1713309200049L;
+  public static final String BUILD_DATE = "2024-04-17 10:29:14 EDT";
+  public static final long BUILD_UNIX_TIME = 1713364154736L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
